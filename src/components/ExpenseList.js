eb4aae301b1d5d7040a86f7481df47ea90957e2e
alsendrha <alsendrha@gmail.com>
2024-01-23 11:23:05 +0900

    완성

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
+import { MdDelete } from 'react-icons/md';
 
-const ExpenseList = ({ initialExpenses, handleDelete }) => {
+const ExpenseList = ({ initialExpenses, expenses, handleDelete, handleEdit, handleClearItem }) => {
   return (
     <>
       <ul className='list'>
         {initialExpenses.map(expense => {
           return (
             <ExpenseItem key={expense.id} expense={expense}
-              handleDelete={handleDelete} />
+              handleEdit={handleEdit} handleDelete={handleDelete} />
           )
         })}
       </ul>
-      <button className='btn'>
-        목록 지우기
-      </button>
+      {expenses.length > 0 ?
+        <button className='btn' onClick={() => handleClearItem()}>
+          목록 지우기
+          <MdDelete className='btn-icon' />
+        </button>
+        : null
+      }
     </>
   )
 }
